4cde268f50960c32a7e98be2ba86932727868088
Scott Hull <ScottHull@users.noreply.github.com>
2016-06-22 16:18:59 -0600

    Update quad2azi.py

--- quad2azi.py
+++ quad2azi.py
@@ -15,6 +15,10 @@ def initialization():
         os.remove("Quad2Adi_Out.csv")
     else:
         pass
+    if "temp.csv" in os.listdir(home_dir_list[0]):
+        os.remove("temp.csv")
+    else:
+        pass
     output_file = open("Quad2Adi_Out.csv", 'a')
     workbook = input(">>> ")
     if os.path.isfile(workbook):
@@ -35,62 +39,65 @@ def initialization():
             raw_vals.append(row[0])
     # print(raw_vals)
     for i in raw_vals:
-        clipped_vals = []
-        clipped_vals.append(int(i[1:-1]))
-        azimuth = []
-        if "N" in i:
-            if "W" in i:
-                val1 = 360 - clipped_vals[0]
-                # print(val1)
-                writethis = i + "," + str(val1)
-                print(writethis)
+        try:
+            clipped_vals = []
+            clipped_vals.append(int(i[1:-1]))
+            azimuth = []
+            if "N" in i:
+                if "W" in i:
+                    val1 = 360 - clipped_vals[0]
+                    # print(val1)
+                    writethis = i + "," + str(val1)
+                    print(writethis)
+                    output_file.write("%s\n" % writethis)
+                elif "E" in i:
+                    val1 = 0 + clipped_vals[0]
+                    # print(val1)
+                    writethis = i + "," + str(val1)
+                    print(writethis)
+                    output_file.write("%s\n" % writethis)
+            elif "S" in i:
+                if "W" in i:
+                    val1 = 180 + clipped_vals[0]
+                    writethis = i + "," + str(val1)
+                    print(writethis)
+                    output_file.write("%s\n" % writethis)
+                elif "E" in i:
+                    val1 = 180 - clipped_vals[0]
+                    writethis = i + "," + str(val1)
+                    print(writethis)
+                    output_file.write("%s\n" % writethis)
+            elif "n" in i:
+                if "w" in i:
+                    val1 = 360 - clipped_vals[0]
+                    # print(val1)
+                    writethis = i + "," + str(val1)
+                    print(writethis)
+                    output_file.write("%s\n" % writethis)
+                elif "e" in i:
+                    val1 = 0 + clipped_vals[0]
+                    # print(val1)
+                    writethis = i + "," + str(val1)
+                    print(writethis)
+                    output_file.write("%s\n" % writethis)
+            elif "s" in i:
+                if "w" in i:
+                    val1 = 180 + clipped_vals[0]
+                    writethis = i + "," + str(val1)
+                    print(writethis)
+                    output_file.write("%s\n" % writethis)
+                elif "e" in i:
+                    val1 = 180 - clipped_vals[0]
+                    writethis = i + "," + str(val1)
+                    print(writethis)
+                    output_file.write("%s\n" % writethis)
+            else:
+                print("\nThere was an error reading: " + i + "!\n")
+                writethis = i + ",ERROR"
                 output_file.write("%s\n" % writethis)
-            elif "E" in i:
-                val1 = 0 + clipped_vals[0]
-                # print(val1)
-                writethis = i + "," + str(val1)
-                print(writethis)
-                output_file.write("%s\n" % writethis)
-        elif "S" in i:
-            if "W" in i:
-                val1 = 180 + clipped_vals[0]
-                writethis = i + "," + str(val1)
-                print(writethis)
-                output_file.write("%s\n" % writethis)
-            elif "E" in i:
-                val1 = 180 - clipped_vals[0]
-                writethis = i + "," + str(val1)
-                print(writethis)
-                output_file.write("%s\n" % writethis)
-        elif "n" in i:
-            if "w" in i:
-                val1 = 360 - clipped_vals[0]
-                # print(val1)
-                writethis = i + "," + str(val1)
-                print(writethis)
-                output_file.write("%s\n" % writethis)
-            elif "e" in i:
-                val1 = 0 + clipped_vals[0]
-                # print(val1)
-                writethis = i + "," + str(val1)
-                print(writethis)
-                output_file.write("%s\n" % writethis)
-        elif "s" in i:
-            if "w" in i:
-                val1 = 180 + clipped_vals[0]
-                writethis = i + "," + str(val1)
-                print(writethis)
-                output_file.write("%s\n" % writethis)
-            elif "e" in i:
-                val1 = 180 - clipped_vals[0]
-                writethis = i + "," + str(val1)
-                print(writethis)
-                output_file.write("%s\n" % writethis)
-        else:
-            print("\nThere was an error reading: " + i + "!\n")
-            writethis = i + ",ERROR"
-            output_file.write("%s\n" % writethis)
-
+        except:
+            print("Error with attitude: " + str(i) + " ...")
+            pass
     output_file.close()
     print("\nDone converting quadrant to azimuth!")
     print("Please see '" + home_dir_list[0] + "' for the output file!\n")
